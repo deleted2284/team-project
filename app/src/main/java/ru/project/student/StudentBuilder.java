4e53eb03f9c02cfff695c99fdc9e7ba163ab79d5
Артющик Анастасия <nastya.2012.v@yandex.ru>
2026-08-23 19:46:00 +0300

    feat(student): add StudentBuilder

--- app/src/main/java/ru/project/student/StudentBuilder.java
+++ app/src/main/java/ru/project/student/StudentBuilder.java
@@ -1,4 +1,26 @@
 package ru.project.student;
 
 public class StudentBuilder {
+    private String groupNumber;
+    private double averageGrade;
+    private String recordBookNumber;
+
+    public StudentBuilder setGroupNumber(String groupNumber) {
+        this.groupNumber = groupNumber;
+        return this;
+    }
+
+    public StudentBuilder setAverageGrade(double averageGrade) {
+        this.averageGrade = averageGrade;
+        return this;
+    }
+
+    public StudentBuilder setRecordBookNumber(String recordBookNumber) {
+        this.recordBookNumber = recordBookNumber;
+        return this;
+    }
+
+    public Student build() {
+        return new Student(groupNumber, averageGrade, recordBookNumber);
+    }
 }
