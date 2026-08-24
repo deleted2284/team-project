8fa66b10d2ff0553a88b881b3e90b81128d7fae8
Ершов Владислав <ershvladislav@gmail.com>
2026-08-24 22:54:19 +0700

    feat: add StringParser and reworking project structure

--- app/src/main/java/ru/project/datasource/FileDataSource.java
+++ app/src/main/java/ru/project/datasource/FileDataSource.java
@@ -7,34 +7,36 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Collector;
-import java.util.NoSuchElementException;
 import ru.project.student.Student;
 import ru.project.collection.MyList;
-import ru.project.collection.MyLinkedList;
 
 public class FileDataSource implements DataSource{
-    private final String fileName;
+    private String fileName;
 
     public FileDataSource(String fileName) {
         this.fileName = fileName;
     }
 
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
     @Override
-    public MyList<Student> create() {
-        MyList<Student> resultList = new MyLinkedList<>();
-        try {
-            StudentCsvReader reader = new StudentCsvReader(fileName);
-            while reader.hasNext() {
-                result.append(reader.next());
-            }
-        }
-        catch (IllegalArgumentException e){
-            System.out.println(e.getMessage());
+    public MyList<Student> fill(int size) {
+        Path path = Paths.get(fileName);
+        try(var lines = Files.lines(path)){
+            return lines.skip(1)
+                    .limit(size)
+                    .map(e -> StringParser.parseString(e))
+                    .filter(e -> !(e == null))
+                    .collect(Collector.of(MyList::new,
+                            MyList::add,
+                            (left, right) -> left.addAll(right)));
+        }catch(FileNotFoundException e){
+            System.out.println("File not found.");
             return null;
-        }
-        catch (NoSuchElementException e){
-            System.out.println("No such element");
-            return null;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
