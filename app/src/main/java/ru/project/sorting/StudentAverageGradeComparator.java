2616603af4256cf4047cf711d88504bec6b264df
Дюрягин Данил <danindeed@yandex.ru>
2026-08-26 16:00:37 +0300

    fix: rename StudentAvarageGradeComparator to StudentAverageGradeComparator

--- app/src/main/java/ru/project/sorting/StudentAverageGradeComparator.java
+++ app/src/main/java/ru/project/sorting/StudentAverageGradeComparator.java
@@ -3,10 +3,10 @@ package ru.project.sorting;
 import ru.project.model.Student;
 import java.util.Comparator;
 
-public class StudentAvarageGradeComparator implements Comparator<Student> {
+public class StudentAverageGradeComparator implements Comparator<Student> {
 
     @Override
     public int compare(Student s1, Student s2) {
-        return Double.compare(s1.getAvarageGrade(), s2.getAvarageGrade());
+        return Double.compare(s1.getAverageGrade(), s2.getAverageGrade());
     }
 }
