3a77ce7d889a6b205cb8bfac8ffd6d7e864399dc
Володин Герман <make_life_better@mail.ru>
2026-08-26 18:58:39 +0400

    style: format Java code with google-java-format

--- app/src/main/java/ru/project/collection/MyLinkedList.java
+++ app/src/main/java/ru/project/collection/MyLinkedList.java
@@ -1,123 +1,120 @@
+package ru.project.collection;
+
 import java.util.Arrays;
-public class MyLinkedList<T> implements MyList<T>
-{
-    private Node<T> head;
-    private int size;
-    public void add(T value) {
-        if (head == null) {
-            this.head = new Node<>(value);
-        } else {
-            Node<T> temp = head;
-            while (temp.getNext() != null) {
-                temp = temp.getNext();
-            }
-            temp.setNext(new Node<>(value));
-        }
-        size++;
-    }
-    public T get(int index)
-    {
-        int currentIndex = 0;
-        Node<T> temp = head;
-        while(temp != null)
-        {
-            if (currentIndex == index)
-            {
-                return temp.getValue();
 
-            }
-            else {
-                temp = temp.getNext();
-                currentIndex++;
-            }
+public class MyLinkedList<T> implements MyList<T> {
+  private Node<T> head;
+  private int size;
 
-        }
-        throw new IllegalArgumentException();
+  public void add(T value) {
+    if (head == null) {
+      this.head = new Node<>(value);
+    } else {
+      Node<T> temp = head;
+      while (temp.getNext() != null) {
+        temp = temp.getNext();
+      }
+      temp.setNext(new Node<>(value));
     }
-    public void remove(int index)
-    {
-        if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException();
-        }
-        if (index == 0)
-        {
-            head = head.getNext();
-            size--;
-            return;
+    size++;
+  }
 
-        }
-        int currentIndex = 0;
-        Node<T> temp = head;
-        while (temp != null)
-        {
-            if (currentIndex == index-1)
-            {
-                temp.setNext(temp.getNext().getNext());
-                size--;
-                return;
-            }
-            else {
-                temp = temp.getNext();
-                currentIndex++;
-            }
-        }
-    }
-    public T set(int index, T value) {
-        if (index < 0 || index >= size) {
-            throw new IllegalArgumentException();
-        }
+  public T get(int index) {
+    int currentIndex = 0;
+    Node<T> temp = head;
+    while (temp != null) {
+      if (currentIndex == index) {
+        return temp.getValue();
 
-        Node<T> temp = head;
-        for (int i = 0; i < index; i++) {
-            temp = temp.getNext();
-        }
+      } else {
+        temp = temp.getNext();
+        currentIndex++;
+      }
+    }
+    throw new IllegalArgumentException();
+  }
 
-        T oldValue = temp.getValue();
-        temp.setValue(value);
-        return oldValue;
+  public void remove(int index) {
+    if (index < 0 || index >= size) {
+      throw new IndexOutOfBoundsException();
     }
-    public int size() {
-        return size;
+    if (index == 0) {
+      head = head.getNext();
+      size--;
+      return;
     }
-    public boolean isEmpty()
-    {
-        return size == 0;
+    int currentIndex = 0;
+    Node<T> temp = head;
+    while (temp != null) {
+      if (currentIndex == index - 1) {
+        temp.setNext(temp.getNext().getNext());
+        size--;
+        return;
+      } else {
+        temp = temp.getNext();
+        currentIndex++;
+      }
     }
-    @Override
-    public String toString() {
-        Object[] res = new Object[size];
-        int index = 0;
-        Node<T> temp = head;
-        while (temp != null) {
-            res[index] = temp.getValue();
-            index++;
-            temp = temp.getNext();
-        }
-        return Arrays.toString(res);
+  }
+
+  public T set(int index, T value) {
+    if (index < 0 || index >= size) {
+      throw new IllegalArgumentException();
     }
 
-    private static class Node<T> {
-        private T value;
-        private Node<T> next;
-
-        public Node(T value) {
-            this.value = value;
-        }
-
-        public void setValue(T value) {
-            this.value = value;
-        }
-
-        public void setNext(Node<T> next) {
-            this.next = next;
-        }
-
-        public T getValue() {
-            return value;
-        }
-
-        public Node<T> getNext() {
-            return next;
-        }
+    Node<T> temp = head;
+    for (int i = 0; i < index; i++) {
+      temp = temp.getNext();
     }
+
+    T oldValue = temp.getValue();
+    temp.setValue(value);
+    return oldValue;
+  }
+
+  public int size() {
+    return size;
+  }
+
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  @Override
+  public String toString() {
+    Object[] res = new Object[size];
+    int index = 0;
+    Node<T> temp = head;
+    while (temp != null) {
+      res[index] = temp.getValue();
+      index++;
+      temp = temp.getNext();
+    }
+    return Arrays.toString(res);
+  }
+
+  private static class Node<T> {
+    private T value;
+    private Node<T> next;
+
+    public Node(T value) {
+      this.value = value;
+    }
+
+    public void setValue(T value) {
+      this.value = value;
+    }
+
+    public void setNext(Node<T> next) {
+      this.next = next;
+    }
+
+    public T getValue() {
+      return value;
+    }
+
+    public Node<T> getNext() {
+      return next;
+    }
+  }
 }
