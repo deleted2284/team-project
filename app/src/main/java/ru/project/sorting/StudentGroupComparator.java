e47732b9ec2d1758912b53de8445418d16248fc0
Володин Герман <make_life_better@mail.ru>
2026-08-28 12:14:10 +0400

    fix: correct student group comparator

--- app/src/main/java/ru/project/sorting/StudentGroupComparator.java
+++ app/src/main/java/ru/project/sorting/StudentGroupComparator.java
@@ -6,7 +6,7 @@ import java.util.Comparator;
 public class StudentGroupComparator implements Comparator<Student> {
 
     @Override
-    public String compare(Student s1, Student s2) {
-        return Integer.compare(s1.getGroupNumber(), s2.getGroupNumber());
+    public int compare(Student s1, Student s2) {
+        return s1.getGroupNumber().compareTo(s2.getGroupNumber());
     }
 }
