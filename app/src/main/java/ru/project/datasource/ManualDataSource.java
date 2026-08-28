a134ffb06bb6db31d71614bcfca731b3bd82f5c8
Владислав Ершов <ershvladislav@gmail.com>
2026-08-28 22:52:52 +0700

    feat: add class FileWriter to append data to file

--- app/src/main/java/ru/project/datasource/ManualDataSource.java
+++ app/src/main/java/ru/project/datasource/ManualDataSource.java
@@ -3,11 +3,25 @@ package ru.project.datasource;
 import java.util.Scanner;
 import ru.project.student.Student;
 import ru.project.collection.MyList;
+import ru.project.collection.MyLinkedList;
 
 public class ManualDataSource implements DataSource{
+    private int size;
+    private MyList<StudentBuffer> bufferMyList = new MyLinkedList<>();
+
+    public ManualDataSource(int size) {
+        this.size = size;
+    }
+
+    public boolean setStudent(
+            int index,
+            String groupNumber,
+            double averageGrade,
+            int recordBookNumber
+    ){}
     @Override
-    public MyList<Student> fill(int size) {
-        MyList<Student> result = new MyList<>();
+    public MyList<Student> create() {
+        MyList<Student> result = new MyLinkedList<>();
         String strToParse;
         Student student;
         Scanner scanner = new Scanner(System.in);
