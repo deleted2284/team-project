e4a48381f0f1915d0eef7f16451685da223d1b3a
Гаджикурбанов Темирлан <temaraili555@gmail.com>
2026-08-29 11:11:12 +0300

    feat:add supplier for MyListCollector

--- app/src/main/java/ru/project/MyListCollector.java
+++ app/src/main/java/ru/project/MyListCollector.java
@@ -1,4 +1,16 @@
+package ru.test.maven.spring;
+import java.util.Collections;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
 public class MyListCollector <T> implements Collector<T, MyList<T>, MyList<T>>
 {
-
+    @Override
+    public Supplier<MyList<T>> supplier()
+    {
+        return () -> new MyLinkedList<>();
+    }
 }
