b932636409c2a5807e66acd47969dc1fff7e0df5
Володин Герман <make_life_better@mail.ru>
2026-08-29 18:37:48 +0400

    refactor(ui): rename BaseWindow to BaseModalWindow

--- app/src/main/java/ru/project/ui/base/BaseModalWindow.java
+++ app/src/main/java/ru/project/ui/base/BaseModalWindow.java
@@ -3,17 +3,17 @@ package ru.project.ui.base;
 import com.googlecode.lanterna.gui2.BasicWindow;
 import com.googlecode.lanterna.gui2.WindowBasedTextGUI;
 
-public abstract class BaseWindow extends BasicWindow {
+public abstract class BaseModalWindow extends BasicWindow {
 
   protected final WindowBasedTextGUI gui;
 
-  protected BaseWindow(String title, WindowBasedTextGUI gui) {
+  protected BaseModalWindow(String title, WindowBasedTextGUI gui) {
     super(title);
 
     this.gui = gui;
   }
 
-  public void show() {
+  public void showModal() {
     gui.addWindowAndWait(this);
   }
 }
