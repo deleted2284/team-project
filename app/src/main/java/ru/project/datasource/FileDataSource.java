bf256e8247f63732d3b2b08bd7c17cab6d37fc1e
Ершов Владислав <ershvladislav@gmail.com>
2026-08-30 22:46:05 +0700

    fix: import java.util.NoSuchElementException into FileDataSource

--- app/src/main/java/ru/project/datasource/FileDataSource.java
+++ app/src/main/java/ru/project/datasource/FileDataSource.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Collector;
+import java.util.NoSuchElementException;
 import ru.project.student.Student;
 import ru.project.collection.MyList;
 import ru.project.collection.MyLinkedList;
